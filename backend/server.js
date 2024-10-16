c79da095322a584b5faf5b058a7fc8abb28ed44c
Mohamed-kassab-96 <111517134+Mohamed-kassab-96@users.noreply.github.com>
2024-10-16 09:03:47 +0300

    4th part completed - socket

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./db/connectToMongoDB.js";
 
-const app = express();
+
 
 dotenv.config();
 const PORT = process.env.PORT || 6333;
 
-app.use(express.json({limit: "50mb"}));
+// app.use(express.json({limit: "50mb"}));
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     res.send("hi from server")
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is Listening on Port ${PORT}`);
     connectDB();
 });

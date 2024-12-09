9a79cd18a6079115d04e17184e6d279de1e12974
Mohamed-kassab-96 <111517134+Mohamed-kassab-96@users.noreply.github.com>
2024-12-09 17:35:15 +0200

    fix routing and vercel configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     // port: 3000,
     proxy: {
       "/api":{
-        target: "http://localhost:6333"
-      }
-    }
+        target: "http://localhost:6333",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   build: {
     outDir: "dist",
